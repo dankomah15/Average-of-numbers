18d3254123cd562044a87416e1e6c26d79eb2c93
Ankomah Daniel <[ankomahdaniel995@gmail.com]>
2025-03-05 08:20:44 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+console.log("Hello world")
+console.log("Hi my name is Daniel")
+
+var accountNumber=1224
+
+console.log(accountNumber)
+
+let contact = 23345
+
+console.log(contact)
+
+const gender = 1
+console.log(gender)
+
+
+
+let name = 1
+let age = 2
+console.log(typeof name + age);
+
+let num1 = 4
+let num2 = 6
+console.log(num1/num2)
+
+
+console.log(new Date());
+const arr = [1,3,4,7,8];
+
+console.log(arr[2]);
+
+//console.log(fruits[2]);
+
+const person ={
+    firstname: "Daniel",
+    lastname: "Ankomah",
+    age: 19,
+    profession: "software Development",
+    skills:["javascript","python","react","Node.js"]
+};
+
+console.log(person.age)
+console.log(person.profession)
+
+
+let firstname = "Ama"
+
+console.log('hello my name is ${firstname}');
+const isTrue = 20 > 10;
+
+console.log(typeof isTrue);
+
+a=34;
+b=34;
+
+console.log( b );
+
+a=100;
+
+while (a > 90) {
+    console.log(a)
+    a=a-1;
+}
+
